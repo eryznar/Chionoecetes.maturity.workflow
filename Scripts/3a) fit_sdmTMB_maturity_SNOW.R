951db66dff209f0c86ece6c1430997d6479290f7
Emily Ryznar <emily.ryznar@nmfs.local>
2026-05-29 15:04:27 -0800

    cleaning up redundancy

--- Scripts/3a) fit_sdmTMB_maturity_SNOW.R
+++ Scripts/3a) fit_sdmTMB_maturity_SNOW.R
@@ -8,52 +8,10 @@
 # LOAD LIBS/PARAMS -----
 source("./Scripts/Sourced scripts/load_libs_params.R")
 
-# LOAD DATA -----
-# Load minima data, calculate cutline params
-snow_minima <- read.csv("./Output/chela_cutline_minima.csv") %>%
-  filter(SPECIES == "SNOW") %>%
-  mutate(BETA0 = coef(lm(MINIMUM ~ MIDPOINT))[1],
-         BETA1 = coef(lm(MINIMUM ~ MIDPOINT))[2])
-
-BETA0 <- unique(snow_minima$BETA0)
-BETA1 <- unique(snow_minima$BETA1)
-
-# Load snow specimen data (subsample 1) and filter appropriately
-snow.specimen<-  readRDS("./Data/snow_survey_specimenEBS.rda")$specimen %>%
-  filter(SHELL_CONDITION == 2, SEX == 1) %>%
-  st_as_sf(., coords = c("LONGITUDE", "LATITUDE"), crs = "+proj=longlat +datum=WGS84") %>%
-  st_transform(., crs = "+proj=utm +zone=2") %>%
-  cbind(st_coordinates(.)) %>%
-  as.data.frame(.) %>%
-  mutate(LATITUDE = Y/1000, # scale to km so values don't get too large
-         LONGITUDE = X/1000,
-         YEAR_F = as.factor(YEAR)) %>%
-  dplyr::select(YEAR, YEAR_F, SIZE, STATION_ID, LATITUDE, LONGITUDE, 
-                SAMPLING_FACTOR) %>%
-  filter(YEAR != 2012) # very little data in this year
-
-# Load snow chela data from chela database (subsample 2)
-snow.chela <-  read.csv("./Data/snow_tanner_cheladatabase.csv") %>% #already filtered appropriately
-  dplyr::select(!X) %>%
-  filter(SPECIES == "SNOW", SIZE >= 35 & SIZE <= 135) %>% # filter to sizes that have variation in maturity
-  mutate(CUTOFF = BETA0 + BETA1*LN_CW, # apply cutline model
-         MATURE = case_when((LN_CH > CUTOFF) ~ 1,
-                            TRUE ~ 0)) %>%
-  st_as_sf(., coords = c("LONGITUDE", "LATITUDE"), crs = "+proj=longlat +datum=WGS84") %>% # make spatial
-  st_transform(., crs = "+proj=utm +zone=2") %>% # transform to utm
-  cbind(st_coordinates(.)) %>%
-  as.data.frame(.) %>%
-  mutate(LATITUDE = Y/1000, # scale to km so values don't get too large
-         LONGITUDE = X/1000,
-         YEAR_F = as.factor(YEAR),
-         YEAR_SCALED = as.numeric(scale(YEAR)),
-         MATURE = case_when((SIZE <=35) ~ 0,
-                            (SIZE >= 135) ~ 1,
-                            TRUE ~ MATURE)) %>%
-  as.data.frame(.) %>%
-  dplyr::select(YEAR, YEAR_F, YEAR_SCALED, STATION_ID, LATITUDE, LONGITUDE, SIZE, MATURE) %>%
-  filter(YEAR != 2012) # very little data in this year
-
+# LOAD CHELA DATA -----
+snow.chela <-  read.csv("./Data/snow_tanner_cheladatabase.csv") %>% #already filtered appropriately with cutline applied
+                  dplyr::select(!X) %>%
+                  filter(SPECIES == "SNOW") 
 
 # FIT MATURITY SDMTMB ----
 # Make mesh
--- Scripts/3b) fit_sdmTMB_maturity_TANNER.R
+++ Scripts/3b) fit_sdmTMB_maturity_TANNER.R
@@ -8,50 +8,10 @@
 # LOAD LIBS/PARAMS -----
 source("./Scripts/Sourced scripts/load_libs_params.R")
 
-# LOAD DATA -----
-# Load minima data, calculate cutline params
-tanner_minima <- read.csv("./Output/chela_cutline_minima.csv") %>%
-  filter(SPECIES == "TANNER") %>%
-  mutate(BETA0 = coef(lm(MINIMUM ~ MIDPOINT))[1],
-         BETA1 = coef(lm(MINIMUM ~ MIDPOINT))[2])
-
-BETA0 <- unique(tanner_minima$BETA0)
-BETA1 <- unique(tanner_minima$BETA1)
-
-# Load tanner specimen data (subsample 1)
-tanner.specimen<-  readRDS("./Data/tanner_survey_specimenEBS.rda")$specimen %>%
-  filter(SHELL_CONDITION == 2, SEX == 1) %>%
-  st_as_sf(., coords = c("LONGITUDE", "LATITUDE"), crs = "+proj=longlat +datum=WGS84") %>%
-  st_transform(., crs = "+proj=utm +zone=2") %>%
-  cbind(st_coordinates(.)) %>%
-  as.data.frame(.) %>%
-  mutate(LATITUDE = Y/1000, # scale to km so values don't get too large
-         LONGITUDE = X/1000,
-         YEAR_F = as.factor(YEAR),
-         YEAR_SCALED = scale(YEAR)) %>%
-  dplyr::select(YEAR, YEAR_F, YEAR_SCALED, DISTRICT, STATION_ID, LATITUDE, LONGITUDE, 
-                SAMPLING_FACTOR) 
-
-# Load tanner chela data from chela database (subsample 2)
-tanner.chela <-  read.csv("./Data/snow_tanner_cheladatabase.csv") %>% #already filtered appropriately
-  dplyr::select(!X) %>%
-  filter(SPECIES == "TANNER", SIZE >= 55 & SIZE <= 145) %>% # filtering ! sizes without separation
-  mutate(CUTOFF = BETA0 + BETA1*LN_CW, # apply cutline model
-         MATURE = case_when((LN_CH > CUTOFF) ~ 1,
-                            TRUE ~ 0)) %>%
-  st_as_sf(., coords = c("LONGITUDE", "LATITUDE"), crs = "+proj=longlat +datum=WGS84") %>%
-  st_transform(., crs = "+proj=utm +zone=2") %>% # transform to UTM
-  cbind(st_coordinates(.)) %>%
-  as.data.frame(.) %>%
-  mutate(LATITUDE = Y/1000, # scale to km so values don't get too large
-         LONGITUDE = X/1000,
-         YEAR_F = as.factor(YEAR),
-         YEAR_SCALED = as.numeric(scale(YEAR)),
-         MATURE = case_when((SIZE <=55) ~ 0,
-                            (SIZE >=145) ~ 1, # or just apply differently to DISTRICTs, though that made the mature plots wonky
-                            TRUE ~ MATURE)) %>%
-  as.data.frame(.) %>%
-  dplyr::select(YEAR, DISTRICT, YEAR_F, YEAR_SCALED, STATION_ID, LATITUDE, LONGITUDE, SIZE,  MATURE) 
+# LOAD CHELA DATA -----
+tanner.chela <-  read.csv("./Data/snow_tanner_cheladatabase.csv") %>% #already filtered appropriately with cutlines applied
+                    dplyr::select(!X) %>%
+                    filter(SPECIES == "TANNER") 
 
 # FIT MATURITY SDMTMB ----
 # Make mesh
